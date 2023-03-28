ceb1826275c28142c773828ae8dba80e5881dc03
Shuntaro Kuroki <shuntaro.kuroki@globis.co.jp>
2023-03-28 11:16:37 +0900

    fetchPolicy

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -6,7 +6,12 @@ import { GetUsersQuery } from '../types/generated/graphql'
 import { GET_USERS } from '../queries/queries'
 
 const FetchMain: VFC = () => {
-  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    // fetchPolicy: 'network-only', // 最新のデータを取得してキャッシュする。通信が完了するまで待つ
+    fetchPolicy: 'cache-and-network', // 最新のデータを取得してキャッシュする。通信が完了するまで待たずにキャッシュを返し、通信が終われば最新の値を返す
+    // fetchPolicy: 'cache-first', // キャッシュを優先的に返す
+    // fetchPolicy: 'no-cache', // 常に最新のデータを取得する
+  })
   if (error) {
     return (
       <Layout title="Hasura fetchPolicy">
